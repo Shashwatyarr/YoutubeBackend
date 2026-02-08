df3baa02277369cf78bc0d0a3aa32102e45ab5ee
Shashwat Srivastava <shashwat.2428it2297@kiet.edu>
2026-02-08 11:30:44 +0530

    utils

--- src/server.js
+++ src/server.js
@@ -4,4 +4,10 @@ import connectDb from "./db/index.js";
 dotenv.config({
   path: "./env",
 });
-connectDb();
+connectDb()
+  .then(
+    app.listen(process.env.PORT || 8000, () => {
+      console.log("Server is running on port:", process.env.PORT);
+    })
+  )
+  .catch((err) => console.log("MongoDb connection failed", err));
